18ae2fd3cead344372362e53707c6e5216b4ae0b
zhonghh13 <zhonghh09@gmail.com>
2015-01-22 19:47:53 +0800

    exception update
    修改merge错误

--- Itui/src/main/java/cn/itui/webdevelop/controller/SearchController.java
+++ Itui/src/main/java/cn/itui/webdevelop/controller/SearchController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import cn.itui.webdevelop.service.CollegeService;
 import cn.itui.webdevelop.service.MajorService;
 import cn.itui.webdevelop.utils.exception.MyNumberFormatException;
-import cn.itui.webdevelop.utils.exception.ParameterAbsenceException;
+import cn.itui.webdevelop.utils.exception.ParameterErrorException;
 
 @Controller
 public class SearchController{
@@ -26,26 +26,26 @@ public class SearchController{
 
 		String tString= request.getParameter("t");
 		if (tString==null) {
-			throw new ParameterAbsenceException();
+			throw ParameterErrorException.getInstance("缺少参数");
 		}
 		int type=Integer.parseInt(tString);
 		String condition = request.getParameter("c");
-		if (condition==null) throw new ParameterAbsenceException();
+		if (condition==null) throw ParameterErrorException.getInstance("缺少参数");
 			
 		String result;
 		if (type==1){
 			//major
 			String category = request.getParameter("cg");
-			if (category==null) throw new ParameterAbsenceException();
+			if (category==null) throw ParameterErrorException.getInstance("缺少参数");
 			String subject = request.getParameter("sj");
-			if (subject==null) throw new ParameterAbsenceException();
+			if (subject==null) throw ParameterErrorException.getInstance("缺少参数");
 			String area = request.getParameter("a");
-			if (area==null) throw new ParameterAbsenceException();
+			if (area==null) throw ParameterErrorException.getInstance("缺少参数");
 			String college_type = request.getParameter("ct");
-			if (college_type==null) throw new ParameterAbsenceException();
+			if (college_type==null) throw ParameterErrorException.getInstance("缺少参数");
 			String major_type = request.getParameter("mt");
-			if (major_type==null) throw new ParameterAbsenceException();
-			if (request.getParameter("l")==null) throw new ParameterAbsenceException();
+			if (major_type==null) throw ParameterErrorException.getInstance("缺少参数");
+			if (request.getParameter("l")==null) throw ParameterErrorException.getInstance("缺少参数");
 			int limit = 0;
 			try{
 				limit = Integer.parseInt(request.getParameter("l"));
@@ -59,7 +59,7 @@ public class SearchController{
 			result = collegeService.searchCollegeList(condition);
 		}else {
 			//error
-			throw new ParameterAbsenceException();
+			throw ParameterErrorException.getInstance("参数错误");
 		}
 		return result;
 	}
